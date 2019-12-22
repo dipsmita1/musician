86990e92c828d814fec03e6860a850c22a52efda
dipsmita1 <dipsmita11@gmail.com>
2019-12-22 22:34:48 +0530

    add healthcheck route

--- routes/musician.js
+++ routes/musician.js
@@ -6,7 +6,7 @@ const schema = require('../store/schema');
 
 // healthcheck
 router.get('/health', (req, res) => {
-  res.status('200').send("Status looks ok!");
+  res.status('200').send("Status looks too ok!");
 });
 
 // retrieve all musicians from data store
